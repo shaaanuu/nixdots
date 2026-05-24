e63e12b8cbaa5cadbeafdb2e4c080d2328ad3afd
shaaanuu <monshan50@gmail.com>
2026-05-24 14:39:27 +0530

    fix: gnome-keyring related issues

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,11 @@ in
     "f+ /var/lib/AccountsService/users/${user}  0600 root root -  [User]\\nIcon=/var/lib/AccountsService/icons/${user}\\n"
     "L+ /var/lib/AccountsService/icons/${user}  -    -    -    -  ${iconPath}"
   ];
-  
+
+  # gnome-keyring
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.sddm.enableGnomeKeyring = true;
+
   # zsh
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
